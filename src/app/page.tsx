e8c48a8dfa44408ddd5f0fe39a63eed0d2f18211
Aniela Fendrych <aniela.fendrych@gmail.com>
2025-02-06 14:00:06 +0100

    Add header, like button, and refactor page structure
    Introduced a `Header` component with dynamic titles and created a `LikeButton` component with state management. Updated the `Home` component structure to display a list of names and included the like button. Simplified layout by removing unused font styles and optimizing imports.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-export default function Home() {
+import LikeButton from "./LikeButton";
+
+function Header({ title }: { title: string }) {
+  return <h1>{title ? title : "Default title"}</h1>;
+}
+
+export default function HomePage() {
+  const names = ["First name", "Second name", "Third name"];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          Napis testowy
-        </div>
-      </main>
+    <div>
+      <Header title="Hello React with TypeScript" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <LikeButton />
     </div>
   );
 }
